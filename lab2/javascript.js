15304ce0433ca2c3d972312806ad0567659e897f
alemandiazcarlos <caleman9791>
2021-09-09 00:02:34 -0400

    ok

---
+++ lab2/javascript.js
@@ -0,0 +1,34 @@
+// var button = document.querySelectorAll(" .tab .button button ");
+// var tabinfo = document.querySelectorAll(".tab .tabinfo button");
+
+// function showinfo(panelIndex, colorCode) {
+//     button.forEach(function(node) {
+//         node.style.backgroundColor = "";
+//         node.style.color = "";
+//     });
+//     button[panelIndex].style.backgroundColor = colorCode;
+//     button[panelIndex].style.color = "white";
+//     tabinfo.forEach(function(node) {
+//         node.style.display = "none";
+//     });
+//     tabinfo[panelIndex].style.display = "block";
+//     tabinfo[panelIndex].style.backgroundColor = colorCode;
+// }
+
+// function showinfo(panelIndex, colorCode) {
+
+//     var button = document.querySelectorAll(".tab .button button");
+//     var tabinfo = document.querySelectorAll(".tab .tabinfo");
+
+//     button.forEach(function(node) {
+//         node.style.backgroundColor = "";
+//         node.style.color = "";
+//     });
+//     button[panelIndex].style.backgroundColor = colorCode;
+//     button[panelIndex].style.color = "white";
+//     tabinfo.forEach(function(node) {
+//         node.style.display = "none";
+//     });
+//     tabinfo[panelIndex].style.display = "block";
+//     tabinfo[panelIndex].style.backgroundColor = colorCode;
+// }
